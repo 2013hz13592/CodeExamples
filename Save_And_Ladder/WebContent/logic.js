3dd16d637f1c8ee6985337edd464a82dce32d1aa
Prasenjit Das <prasenjit.das@gmail.com>
2016-05-29 02:06:43 +0530

    Added More Tasks

--- Save_And_Ladder/WebContent/logic.js
+++ Save_And_Ladder/WebContent/logic.js
@@ -1,15 +1,18 @@
-var c= document.getElementById("blob");
-var b= c.getContext("2d");
-var noOftasks = 0;
+var c=document.getElementById("blob");
+var b=c.getContext("2d");
 c.width=378;
 c.height=378;
+
+var noOftasks=0;
 var x=0;
-var speed = 2;
-var curr_x = c.width - 30 ,curr_y = c.height-30;
-var o_x = curr_x,o_y = curr_y;
-var background = new Image();
-background.src = "./GameBoard.png";
-background.onload = function(){
+var speed=2;
+//var curr_x=c.width - 30, curr_y=c.height-30;
+var curr_x=c.width - 368, curr_y=c.height-28;
+var o_x=curr_x, o_y=curr_y;
+var background=new Image();
+
+background.src="./GameBoard.png";
+background.onload=function(){
     b.drawImage(background,0,0);
 }
 var dist = 0;
@@ -26,34 +29,70 @@ var Draw = function(){
 update();
 function update()
 {
-    b.drawImage(background,0,0);
-    Draw();
-   if(noOftasks<=0){
-      
-   };  
+	b.drawImage(background,0,0);
+	Draw();
+	if(noOftasks<=0){
+		
+	};  
    
-   if(noOftasks ==1){
-    if(Math.abs(curr_y - o_y) <= 38 ){
-        curr_y = curr_y-speed;
-    }
-   }
-   if(noOftasks == 2){
-    if(Math.abs(curr_x - o_x) <= 264){
-        curr_x = curr_x - speed;
-        curr_y = curr_y;
-    }
-   } 
-   if(noOftasks == 3){     
-    if(dist < 75){
-        curr_y = curr_y - 1;
-        curr_x = curr_x +0.5;
-    }
-    dist++;
-   }
-   if(noOftasks == 4){
-    if( curr_x < 272 ){
-        curr_x = curr_x + speed;
-    }
-   }
-    window.requestAnimationFrame(update);
+	//if(noOftasks==1){
+	//	if(Math.abs(curr_y - o_y) <= 38 ){
+	//		curr_y=curr_y-speed;
+	//	}
+	//}
+	
+	if(noOftasks==1){
+		if(Math.abs(curr_x - o_x) <= 185 ){
+			curr_x=curr_x+speed;
+		}
+	}
+   
+   //if(noOftasks==2){
+	//	if(Math.abs(curr_x - o_x) <= 264){
+	//	   curr_x=curr_x-speed;
+	//	   curr_y=curr_y;
+	//}
+   //}
+	
+	if(noOftasks==2){
+		if(Math.abs(curr_x - o_x) <= 338){
+		   curr_x=curr_x+speed;
+		   curr_y=curr_y;
+		}
+		if(Math.abs(curr_x - o_x) > 338){
+			if(Math.abs(curr_y - o_y) <= 35 ){
+				curr_y=curr_y-speed;
+			}
+		}
+    } 	
+   
+	//if(noOftasks==3){     
+	//	if(dist < 75){
+	//		curr_y = curr_y - 1;
+	//		curr_x = curr_x +0.5;
+	//	}
+	//	dist++;
+	//}
+	
+	if(noOftasks==3){     
+		if(Math.abs(curr_x - o_x) >= 78){
+			   curr_x=curr_x-speed;
+			   curr_y=curr_y;
+		}
+	}
+   
+	if(noOftasks==4){     
+		if(dist < 75){
+			curr_y = curr_y - 1;
+			curr_x = curr_x +0.5;
+		}
+		dist++;
+	}
+	
+   /*if(noOftasks==4){
+	   if( curr_x < 272 ){
+		   curr_x = curr_x + speed;
+	   }
+   }*/
+   window.requestAnimationFrame(update);
 }
